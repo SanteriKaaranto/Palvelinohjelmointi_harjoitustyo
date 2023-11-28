9e56b6e18111e9d128639ea5baf142cea935050d
Santeri <santeri.kaaranto@gmail.com>
2023-11-28 15:19:23 +0200

    json testaus

--- src/main/java/com/example/harjoitustyo/HarjoitustyoApplication.java
+++ src/main/java/com/example/harjoitustyo/HarjoitustyoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HarjoitustyoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HarjoitustyoApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/harjoitustyo/Item.java
@@ -0,0 +1,15 @@
+package com.example.harjoitustyo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private String name;
+    private String count;
+    private int id;
+}
