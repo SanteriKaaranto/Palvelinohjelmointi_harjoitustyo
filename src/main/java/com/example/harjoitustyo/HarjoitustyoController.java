2793e011e31d0a059a137b1985635a17bee98611
Santeri <santeri.kaaranto@gmail.com>
2023-12-04 22:02:00 +0200

    database sisältö näkyy html sivulla ja poistettiin turhat asiat

--- src/main/java/com/example/harjoitustyo/HarjoitustyoController.java
+++ src/main/java/com/example/harjoitustyo/HarjoitustyoController.java
@@ -1,54 +1,31 @@
 package com.example.harjoitustyo;
 
-import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 @Controller
 public class HarjoitustyoController {
 
-    //@Autowired
-    //private NoteRepository noteRepository;
+    @Autowired
+    private NoteRepository noteRepository;
     
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("teksti", "Hei mualima!");
+        model.addAttribute("notes", this.noteRepository.findAll());
         return "index";
     }
 
-    private String message;
+/*     private String message;
     // 'content' pitää vastaa HTML:n name='' osiota
     @PostMapping("/")
     public String posting(@RequestParam String content) { 
         this.message = content;
         System.out.println(message);
         return "redirect:/";
-    }
-
-    // json testi
-   @GetMapping("/json")
-    public String getJsonData(Model model) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ClassPathResource resource = new ClassPathResource("db.json");
-        JsonNode rootNode = objectMapper.readTree(resource.getInputStream());
-        // Assuming "items" is the property name in your JSON
-        JsonNode itemsNode = rootNode.get("items");
-        // Deserialize the list of items
-        List<Item> itemList = objectMapper.readValue(itemsNode.toString(), new TypeReference<List<Item>>() {});
-        model.addAttribute("jsonData", itemList);
-
-        return "json";
-    }
+    } */
 }
 
