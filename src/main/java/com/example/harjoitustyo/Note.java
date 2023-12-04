6aaabe8a6d75ee936f3f9684c89fb66f56ef459a
Santeri <santeri.kaaranto@gmail.com>
2023-12-04 17:28:22 +0200

    test

--- src/main/java/com/example/harjoitustyo/Note.java
+++ src/main/java/com/example/harjoitustyo/Note.java
@@ -7,19 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "notes")
 public class Note implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name="note")
     private String note;
 }
