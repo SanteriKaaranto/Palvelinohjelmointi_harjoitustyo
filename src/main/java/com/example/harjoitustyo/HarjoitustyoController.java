9c097106b11104ff1ba4917bcc63148d302b78e3
Santeri <santeri.kaaranto@gmail.com>
2023-12-05 13:12:51 +0200

    Poisto toiminnallisuus

--- src/main/java/com/example/harjoitustyo/HarjoitustyoController.java
+++ src/main/java/com/example/harjoitustyo/HarjoitustyoController.java
@@ -1,6 +1,5 @@
 package com.example.harjoitustyo;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,23 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class HarjoitustyoController {
 
     @Autowired
     private NoteRepository noteRepository;
-    
+
+    // Get
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("notes", this.noteRepository.findAll());
         return "index";
     }
 
+    // Post
     @PostMapping("/saveNote")
     public String saveNote(@RequestParam String note) {
         noteRepository.save(new Notes(null, note));
         return "redirect:/";
     }
-}
 
+    // Delete
+    @GetMapping("/deleteNote")
+    public String deleteNote(@RequestParam Long id) {
+        noteRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
